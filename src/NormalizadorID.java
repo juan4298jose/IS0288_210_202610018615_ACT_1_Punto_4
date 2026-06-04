8eb6fe281412b67b4a93b8bd11e23d4149a7e055
juan4298jose <juanjosediazlop8@gmail.com>
2026-06-04 20:53:02 +0000

    cambios realizados

--- src/NormalizadorID.java
+++ src/NormalizadorID.java
@@ -1,22 +1,22 @@
 public class NormalizadorID{
     public static void main(String[] args) {
-        String idSucio = " User_123 ";
-        String idLimpio = idSucio.trim().toUpperCase().substring(0, 5); 
+        String idSucio = " user_123 ";
+        String idLimpio = idSucio.trim().toUpperCase().substring(0, 8); 
 
-         //System.identityHashCode("ID original" +idSucio);
+         System.identityHashCode("ID original" +idSucio);
          System.identityHashCode("ID limpio"  +idLimpio);
 
          System.out.println("\nHash ID Original: " + System.identityHashCode(idSucio));
-           System.out.println("\nHash ID Original: " +idSucio);
+           System.out.println(" ID Original: " +idSucio);
 
-        System.out.println("Hash ID Limpio: "+ System.identityHashCode(idLimpio));
+        System.out.println("Hash ID Limpio: " + System.identityHashCode(idLimpio));
 
         StringBuilder inversor = new StringBuilder(idLimpio);
         inversor.append(idSucio);
 
         String idInvertido = inversor.reverse().toString();
  
- System.out.println("Hash ID Invertido: " + idInvertido);
+ System.out.println(" ID Invertido: " + idInvertido);
 
 
     }
